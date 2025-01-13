64883e54211148ecaaf28b778e578d86b12aa45d
solalohana <solalohana@gmail.com>
2025-01-13 13:57:27 +0200

    added Timeout interceptors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { HttpExceptionFilter } from './common/filters/http-exception/http-except
 import { ApiKeyGuard } from './common/guards/api-key/api-key.guard';
 import { wrap } from 'module';
 import { WrapResponseInterceptor } from './common/interceptors/wrap-response/wrap-response.interceptor';
+import { TimeoutInterceptor } from './common/interceptors/timeout/timeout.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -18,7 +19,7 @@ async function bootstrap() {
     },
   }));
   app.useGlobalFilters(new HttpExceptionFilter());
-  app.useGlobalInterceptors(new WrapResponseInterceptor());
+  app.useGlobalInterceptors(new WrapResponseInterceptor(), new TimeoutInterceptor());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
